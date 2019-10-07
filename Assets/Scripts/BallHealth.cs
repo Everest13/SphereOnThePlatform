38f009f8cea0a88206a77dfa7b766eaae8b15927
Max Tsoy <protosha123@gmail.com>
2019-10-07 21:49:11 +0300

    SOP - Add importing Skybox and BallHealth script behavior

---
+++ Assets/Scripts/BallHealth.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class BallHealth : MonoBehaviour
+{
+    public int maxFallDistance = -1;
+    public GameManager gameManager;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y <= maxFallDistance)
+        {
+            gameManager.Restart();
+        }
+    }
+
+}
