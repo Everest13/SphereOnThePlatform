04a43df306ed01cb2ebec8bfdfc13b859f801db7
Max Tsoy <protosha123@gmail.com>
2019-10-07 13:58:46 +0300

    SOP - init masterx

---
+++ Assets/Scripts/BallControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    public int rotationSpeed = 100;
+    public int jumpHeight = 8;
+    private bool isFalling = false;
+
+    public Rigidbody rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Добавляет крутящий момент при нажатии "a"/"d" - Handle ball roration
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        rotation *= Time.deltaTime;
+        rb.AddRelativeTorque(Vector3.back * rotation);
+
+        //
+
+    }
+}   
